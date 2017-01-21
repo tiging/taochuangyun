802b5a8a7690f1fdf8924237ccd8763f57ca3030
mmcing <admin@mmcing.com>
2017-01-21 20:44:38 +0800

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+/*
+* @Author: Administrator
+* @Date:   2017-01-21 18:44:55
+* @Last Modified by:   Administrator
+* @Last Modified time: 2017-01-21 19:21:57
+*/
+
+'use strict';
+var gulp = require('gulp');
+gulp.task('style',function(){
+	gulp.src('src/styles/*.less')
+	.pipe()
+
+});
---
+++ src/scripts/app.js
@@ -0,0 +1,8 @@
+/*
+* @Author: Administrator
+* @Date:   2017-01-21 18:52:07
+* @Last Modified by:   Administrator
+* @Last Modified time: 2017-01-21 19:21:57
+*/
+
+'use strict';
